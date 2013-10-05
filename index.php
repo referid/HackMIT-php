707a4df2956a8fcf645f14b9136f8c9c5d19b281
C Friesen <christine.friesen@mail.mcgill.ca>
2013-10-05 15:13:47 -0700

    try database connection

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php
+echo <<<_END
 <html>
     <head>
         <link href="layout/css/core.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div id="main" class="center">
-            <?php include('/layout/content.php'); ?>
+_END;
+    include('layout/content.php');
+echo <<<_END
         </div>
     </body>
 </html>
+_END;
