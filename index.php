b783ca52a6178461f18a895e35f92002bd1c5bc6
C Friesen <christine.friesen@mail.mcgill.ca>
2013-10-05 16:21:29 -0700

    with requireds

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-echo "boo";
-
-echo "boo";
+echo "start of index.php";
 echo <<<_END
 <html>
     <head>
@@ -11,30 +9,20 @@ echo <<<_END
         <div id="main" class="center">
 _END;
 
-echo $_GET['uid'];
-
-echo $_GET['company'];
-
  if (isset($_GET['uid']) && isset($_GET['company'])) {
-    $id = sanitizeString($_GET['uid']);
-    echo "received id: " . $id;         //!!!! need to regex to check input
-    $database = sanitizeString($_GET['company']);    //!!!! need to regex to check input
+    $id = sanitizeString($_GET['uid']); //!!!! need to regex to check input
+    echo "received id: " . $id;
+    $database = sanitizeString($_GET['company']); //!!!! need to regex to check input
     echo "received company: " . $company;
  }
 
+require_once 'lib/PHP-on-Couch/lib/couch.php';
+require_once 'lib/PHP-on-Couch/lib/couchClient.php';
+require_once 'lib/PHP-on-Couch/lib/couchDocument.php';
 
- /* sanitizeString is a function that is intended to sanitize input gathered
- * from forms in order to prevent injection and cross site scripting
- * $str_input is the string retrieved*/
-function sanitizeString($str_input) {
-    $str_input = strip_tags($str_input);
-    $str_input = htmlentities($str_input);
-    $str_input = stripslashes($str_input);
-    return $str_input;
-}
+echo "after required statements";
 
 
- echo "hello there";
 /*
     try {
         $client = new couchClient ('http://localhost:5984', 'company');
@@ -54,6 +42,19 @@ function sanitizeString($str_input) {
     }
     echo "exit try statement";
       */
+
+
+ /* sanitizeString is a function that is intended to sanitize input gathered
+ * from forms in order to prevent injection and cross site scripting
+ * $str_input is the string retrieved*/
+function sanitizeString($str_input) {
+    $str_input = strip_tags($str_input);
+    $str_input = htmlentities($str_input);
+    $str_input = stripslashes($str_input);
+    return $str_input;
+}
+
+echo "end of index.php";
 echo <<<_END
         </div>
     </body>
