b0008282298fb431bf53945bac9fe963cbde8fbe
Trevor Stanhope <trevor@T510>
2013-10-06 06:50:17 -0400

    retry

--- history.php
+++ history.php
@@ -36,7 +36,7 @@ try {
          printf("<div>
                  <ul>");
          echo $doc->history;
-         $json = utf8_encode($doc->history);
+         $json = $doc->history;
          $productList = json_decode($json,true);
          echo $productList;
          foreach ($productList as $name => $id) {
