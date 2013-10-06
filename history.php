fff4792c5286eb1f26999da5c31d7a4a21bf9bfa
Trevor Stanhope <trevor@T510>
2013-10-06 08:06:28 -0400

    PRINTFFFFFFFF

--- history.php
+++ history.php
@@ -38,7 +38,7 @@ try {
          $json = $doc->history;
          foreach($json as $value) {
            $split = explode('/', $value);
-           print("<li>%s</li>", $split[0]);
+           printf("<li>%s</li>", $split[0]);
          }
          printf("</ul>
                 </div>");
